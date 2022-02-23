900b00fc95b6fac90fa1120b9cc1484640b46f7e
Alisa <djalise@gmail.com>
2022-02-23 23:07:59 +0200

    Update

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 
 <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Person Registry</title>
 </head>
 <body>
-<header class="header">
-    <h1 class="title">Person Registry</h1>
-</header>
-
-<form action="app/addPerson.php" method="post">
-    <input type="text" name="name" placeholder="Name">
-    <input type="text" name="surname" placeholder="Surname">
-    <input type="number" name="personal_code" placeholder="Personal code">
-    <br>
-    <button type="submit" name="submit">Add person</button>
-</form>
-
+<div class="header">
+    <div class="title">
+        <h1>Person Registry</h1>
+    </div>
+</div>
+<div class="form">
+    <form action="app/addPerson.php" method="post">
+        <input type="text" class="input" name="name" placeholder="Name">
+        <input type="text" class="input" name="surname" placeholder="Surname">
+        <input type="number" class="input" name="personal_code" placeholder="Personal code">
+        <br>
+        <button type="submit" class="button" name="submit">Add person</button>
+    </form>
+</div>
 </body>
